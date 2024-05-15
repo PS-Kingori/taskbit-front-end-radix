799f31c256cfb2d575f07e3f2c1aa2045abbc909
PS-Kingori <peterskingori@gmail.com>
2024-05-15 16:33:37 +0300

    deploy test

--- src/herosec.jsx
+++ src/herosec.jsx
@@ -24,7 +24,7 @@ const Herosec = () => {
           </div>
         </div>
         <div className="w-full sm:w-1/2 flex  ">
-         <img src="/vite.svg" alt="image" />
+         <img src="/hero.gif" alt="" />
         </div>
 
       </div>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/taskbit-front-end-radix/',
   base: '/',
 })
